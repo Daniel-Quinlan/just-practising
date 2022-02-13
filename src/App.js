984bdd1bda14f644fa5293176ef9cfba73f02807
Daniel Quinlan <daquinlan@iinet.net.au>
2022-02-13 21:20:55 +1100

    fin app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import Card from "./Components/UI/Card";
 import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div className="w-screen h-screen bg-background pt-12">
       <Card>
-        <AddUser />
+        <AddUser onAddUser={addUserHandler} />
       </Card>
+      <UsersList users={usersList} />
     </div>
   );
 }
